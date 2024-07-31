c0c748c579fd122d078a7cacbb45a9bde4bee4dd
Stephen Taylor <stephenmtaylor93@gmail.com>
2024-07-31 11:58:19 -0400

    work

---
+++ Exercises/easy_2/drop_while.rb
@@ -0,0 +1,10 @@
+def drop_while(arr)
+  arr.each_with_object([]){|element, output| output << element if !yield(element) }
+end
+
+p drop_while([1, 3, 5, 6]) { |value| value.odd? } == [6]
+p drop_while([1, 3, 5, 6]) { |value| value.even? } == [1, 3, 5, 6]
+p drop_while([1, 3, 5, 6]) { |value| true } == []
+p drop_while([1, 3, 5, 6]) { |value| false } == [1, 3, 5, 6]
+p drop_while([1, 3, 5, 6]) { |value| value < 5 } == [5, 6]
+p drop_while([]) { |value| true } == []
