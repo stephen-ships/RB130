7e5ba5dcda37c5e606f7c088ae69a518a0cf04b7
Stephen Taylor <stephenmtaylor93@gmail.com>
2024-08-08 17:46:57 -0400

    practice

--- Exercises/medium_1/text_analyzer.rb
+++ Exercises/medium_1/text_analyzer.rb
@@ -2,10 +2,28 @@ class TextAnalyzer
   def process
     filename = "sample_text.txt"
     file = File.open(filename)
+    count = 0
+    count = file.read.scan(yield).size
+    # file.each_line do |line|
+    # end
+    puts count
   end
 end
 
 analyzer = TextAnalyzer.new
-analyzer.process { # your implementation }
-analyzer.process { # your implementation }
-analyzer.process { # your implementation }
+analyzer.process { /^\n/ }
+analyzer.process { /^/ }
+analyzer.process { /\b\w+\b/ }
+
+class TextAnalyzer
+  def process
+    file = File.open('sample_text.txt', 'r')
+    yield(file.read)
+    file.close
+  end
+end
+
+analyzer = TextAnalyzer.new
+analyzer.process { |text| puts "#{text.split("\n\n").count} paragraphs" }
+analyzer.process { |text| puts "#{text.split("\n")} lines" }
+analyzer.process { |text| puts "#{text.split(' ').count} words" }
